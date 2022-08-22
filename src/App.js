1ca471c3fc66fd2f20d21f2ca4f8d9d395593c4b
Madison Funderburk <madfun12@gmail.com>
2022-08-21 21:39:42 -0500

    styling and console intro text change

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 function App() {
   const inputRef = React.useRef(null)
   React.useEffect(() => {inputRef.current.select()},[inputRef])
+  
   return (
     <div className="console">
       <pre>
       <h5>madison funderburk all rights reserved...</h5>
-      <h1>       __    __    _    __   ____  _   _  _   _ </h1>
-      <h1>//////|  \  /  |  / \  |  \ | ___|| | | || \ | |/////////////</h1>
-      <h1>//////| | \/ | | / ^ \ | | \| __| | |_| ||  \| |/////////////</h1>
-      <h1>//////|_|    |_|/_/ \_\|__ /|_|   |_____||_|\__|/////////////</h1>
+      <h1>       __   __    _    __   ____  _   _  _   _ </h1>
+      <h1>//////|  \_/  |  / \  |  \ | ___|| | | || \ | |/////////////</h1>
+      <h1>//////| |\_/| | / ^ \ | | \| __| | |_| ||  \| |/////////////</h1>
+      <h1>//////|_|   |_|/_/ \_\|__ /|_|    \___/ |_|\__|/////////////</h1>
       </pre>
       <div className="console-input-box">
         <p className="green">~ guest@madisonfunderburk.com:</p>
